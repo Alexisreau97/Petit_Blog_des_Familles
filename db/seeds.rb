397f6be932fe3d9b5fed40e117ce1dae8f469797
MelloFlo <florian_pinto@outlook.fr>
2018-10-24 17:35:09 +0200

    add comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,27 @@
 
 require 'faker'
 
-# 9.times do
-#   user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email)
-# end
-
-4.times do
-	categorie = Categorie.create!(name: Faker::Book.genre)
+# Cette methode permet de générer aléatoirement 10 utilisateurs avec un nom, un prénom et une adresse email.
+9.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email)
 end
 
+# Cette methode permet de générer aléatoirement 5 categories avec un nom.
+4.times do
+  categorie = Categorie.create!(name: Faker::Book.genre)
+end
+
+# Cette methode permet de générer aléatoirement 10 articles avec un titre, un contenu, l'id de l'auteur et l'id de la categorie.
 9.times do
   article = Article.create!(title: Faker::Book.title, content: Faker::FamousLastWords.last_words, user_id: 4, categorie_id: 2)
 end
 
+# Cette methode permet de générer aléatoirement 15 commentaires avec l'id de l'utilisateur et l'id de l'article.
+14.times do
+  comment = Comment.create!(content: Faker::Hobbit.quote, user_id: 4, article_id: 2)
+end
+
+# Cette methode permet de générer aléatoirement 15 likes avec l'id de l'utilisateur et l'id de l'article.
+14.times do
+	like = Like.create!(user_id: 3, article_id: 5)
+end
